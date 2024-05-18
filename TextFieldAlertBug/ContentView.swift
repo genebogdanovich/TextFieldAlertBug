0c635f1179149c4c32593effbbec18777104a2aa
Gene Bogdanovich <genebogdanovich@gmail.com>
2024-05-18 08:42:14 +0300

    Initial Commit

---
+++ TextFieldAlertBug/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TextFieldAlertBug
+//
+//  Created by Gene Bogdanovich on 18.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TextFieldAlertBug/TextFieldAlertBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldAlertBugApp.swift
+//  TextFieldAlertBug
+//
+//  Created by Gene Bogdanovich on 18.05.24.
+//
+
+import SwiftUI
+
+@main
+struct TextFieldAlertBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
